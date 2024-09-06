a0cb16626c19a9d131b4836d0cebd667e0f35138
Sagar Kosalge <sskosalge@gmail.com>
2024-09-06 06:20:23 +0530

    code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,41 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {user} from "../models/user.model.js"
 
 const registerUser = asyncHandler( async (req, res)=> {
-    res.status(200).json({
-        message: "Hii Sagar"
-    })})
+    // get user details from frontend
+    //validation - not empty
+    // check if user already exist: username,mail
+    // check for images, check for avatar
+    // upload them in cloudnary- take ref for file uploaded
+    // avatar uploaded or not
+    // create user object- create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return response
+
+    const{fullName, email, username, password}=req.body
+    console.log("email: ", email);
+
+    // if(fullName === ""){
+    //     throw new ApiError("400", "fullname is required")
+    // }
+    if(
+        [fullName, email, username, password].some((field)=>
+    field?.trim()=== "")
+)
+    {
+        throw ApiError(400, "All fields are required")
+    }
+    const existedUser = User.findOne({
+        $or: [{username}, {email}]
+    })
+    if(existedUser){
+        throw new ApiError("409", "User with email or username already exist")
+    }
+// we get req.body due to express but we req.files due to multer
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    req.files
+})
 
 export {registerUser,}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import {Router} from "express"
 const router = Router()
 import { registerUser } from "../controllers/user.controller.js"
+import {upload} from "../middlewares/multer.middeleware.js"
 
-router.route("/register").post(registerUser)
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name: avatar,
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 // here url will become http://localhost:8000/api/v1/users/register
 // /users from app.js from src and /register from this file this better practice rather than using just app.post use 2 separte files one for routes and one for a middleware to call route
 
