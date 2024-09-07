5ea13c4b964f66a2db15495f7c987332ac38d219
Sagar Kosalge <sskosalge@gmail.com>
2024-09-07 17:48:02 +0530

    added register user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import {user} from "../models/user.model.js"
+import {uploadOnCloudnary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler( async (req, res)=> {
     // get user details from frontend
@@ -35,7 +37,34 @@ const registerUser = asyncHandler( async (req, res)=> {
     }
 // we get req.body due to express but we req.files due to multer
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    req.files
+    const coverImageLocalPath = req.files?.coverimage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is required")
+    }
+    const avatar = await uploadOnCloudnary(avatarLocalPath)
+    const coverImage = await uploadOnCloudnary(coverImageLocalPath)
+    if(!avatar){
+        throw new ApiError(400, "Avatar file is required")
+        
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+    const createdUser = await User.findById(user._id).select("-password -refreshToken")
+    
+    if(!createdUser){
+        throw new ApiError(500, "User not created")
+    }
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
 })
 
 export {registerUser,}
